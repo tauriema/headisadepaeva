6fd6ff213a9184333dd618f0e1c2bdcd3af5d617
Passive Club - Kasre Diss Track II (Christmas Special) <karlpulges@gmail.com>
2026-03-18 07:01:23 +0000

    miski

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Võimaldab JSON-i päringute kehas / Allow JSON in request bodies
 app.use(express.json());
 
-// Peamine marsruut / Main route
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World' });
+// Avaleht
+app.get("/", (req, res) => {
+  res.json({ message: "koduleht" });
+});
+
+// Login leht
+app.get("/login", (req, res) => {
+  res.json({ message: "login" });
+});
+
+// Leht kus saab arvustusi vaadata
+app.get("/arvustused", (req, res) => {
+  res.json({ message: "arvustuste vaatamise leht" });
+});
+
+// Leht kus saab arvustusi anda
+app.get("/annaarvustus", (req, res) => {
+  res.json({ message: "arvustuste andmise leht" });
+});
+
+app.get("/reklaamivabariik", (req, res) => {
+  res.json({ message: "reklaamide spam" });
 });
 
 // Server kuulab pordil 3000 / Server listens on port 3000
 app.listen(3000, () => {
-  console.log('Server töötab pordil 3000 / Server is live on port 3000');
+  console.log("Server töötab pordil 3000 / Server is live on port 3000");
 });
