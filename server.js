d992ea4c7110189aa6e9d149f84cb0b7edb70ba8
Passive Club - Kasre Diss Track II (Christmas Special) <karlpulges@gmail.com>
2026-03-19 06:31:35 +0000

    villem

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/annaarvustus", (req, res) => {
 });
 
 app.get("/reklaamivabariik", (req, res) => {
-  res.json({ message: "reklaamide spam ja tere" });
+  res.json({ message: "reklaamide spam" });
 });
 
 // Server kuulab pordil 3000 / Server listens on port 3000
